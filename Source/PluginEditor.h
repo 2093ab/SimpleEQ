ee2d57035599c6cca7ac6250aa54522e0c82f4f1
2093ab <gil2093ab@gmail.com>
2022-05-31 10:17:42 +0900

    Created ResponseCurveComponent

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -19,12 +19,32 @@ struct CustomRotarySlider : juce::Slider
         
     }
 };
+
+struct ResponseCurveComponent : juce::Component,
+juce::AudioProcessorParameter::Listener,
+juce::Timer
+{
+    ResponseCurveComponent(SimpleEQAudioProcessor&);
+    ~ResponseCurveComponent();
+    
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+    
+    void parameterGestureChanged (int parametrIndex, bool gestureIsStarting) override { }
+    
+    void timerCallback() override;
+    
+    void paint (juce::Graphics& g) override;
+private:
+    SimpleEQAudioProcessor& audioProcessor;
+    juce::Atomic<bool> parametersChanged { false };
+    
+    MonoChain monoChain;
+};
+
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
-juce::AudioProcessorParameter::Listener,
-juce::Timer
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -33,18 +53,11 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-    
-    void parameterValueChanged (int parameterIndex, float newValue) override;
-    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { };
-    
-    void timerCallback() override;
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
 
-    juce::Atomic<bool> parametersChanged { false };
-    
     CustomRotarySlider peakFreqSlider,
     peakGainSlider,
     peakQualitySlider,
@@ -52,6 +65,8 @@ private:
     highCutFreqSlider,
     lowCutSlopeSlider,
     highCutSlopeSlider;
+    
+    ResponseCurveComponent responseCurveComponent;
     
     using APVTS = juce::AudioProcessorValueTreeState;
     using Attachment = APVTS::SliderAttachment;
@@ -65,8 +80,6 @@ private:
                 highCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
-    
-    MonoChain monoChain;
-    
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
